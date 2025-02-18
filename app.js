2982593d6c48e9cf8f0a9a67733c314bb9922abb
Juan M. Torres Ramírez <jtorresr1403@gmail.com>
2025-02-18 17:27:08 -0500

    Logica sorteo de amigo secreto

--- app.js
+++ app.js
@@ -1,1 +1,42 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+let listaAmigos = document.getElementById("listaAmigos");
+let resultado = document.getElementById("resultado");
+
+function agregarAmigo() {
+    let nombreAmigo = document.getElementById("amigo").value;
+
+    if (nombreAmigo) {
+
+        amigos.push(nombreAmigo);
+        listaAmigos.innerHTML = "";
+
+        for (let index = 0; index < amigos.length; index++) {
+            let item = document.createElement('li');
+            item.textContent = amigos[index];
+            listaAmigos.appendChild(item);
+        }
+
+        limpiarInput();
+
+    } else {
+        alert("Por favor, inserte un nombre.");
+    }
+}
+
+function sortearAmigo() {
+
+    if (amigos.length != 0) {
+        let index = Math.floor(Math.random() * amigos.length);
+        let amigoElegido = amigos[index];
+        resultado.innerHTML = `El amigo secreto es ${amigoElegido}`;
+    }else{
+        alert("Por favor, inserte nombre de amigos a la lista.");
+    }
+
+}
+
+function limpiarInput() {
+    document.getElementById("amigo").value = "";
+}
+
